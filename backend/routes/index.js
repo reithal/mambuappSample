c32dbf7ff431dafeff7a3b643f9a6145b584f662
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2024-04-12 16:54:50 -0400

    Changes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -21,6 +21,9 @@ router.post('/', (req, res) => {
       dynamicHTML = "<html><style>h1 {color: #73757d;}</style><body><h1>Loan Info Mambu App</h1>";
       dynamicHTML += `<p>Welcome user encodedKey: ${check.data.USER_KEY}</p>`;
       dynamicHTML += `<p>You have accesed the entity ID: ${check.data.OBJECT_ID} from ${check.data.DOMAIN}</p>`;
+      dynamicHTML += `<p style="width:800px; word-wrap:break-word; display:inline-block;">The body of request received is ${JSON.stringify(req.body)}</p>`
+      dynamicHTML += `<p>The PART1 of request received is ${req.body.signed_request.split('.')[0]}`
+      dynamicHTML += `<br style="width:800px; word-wrap:break-word; display:inline-block;">The PART2 of request received is ${req.body.signed_request.split('.')[1]}</p></html>`;
       res.send(dynamicHTML);
     } else {
       res.sendStatus(401);
